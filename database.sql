e31d4df4bf908f57d726a7fa7910c508ad1f778c
Spolito24 <[baptisterenier69@gmail.com](mailto:baptisterenier69@gmail.com)>
2023-04-27 10:27:27 +0200

    some fix for the connexion page

--- database.sql
+++ database.sql
@@ -62,40 +62,15 @@ ALTER TABLE `item`
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
-
-
 USE projet_2;
 
 SELECT * FROM article;
-
 TRUNCATE TABLE article;
 DROP TABLE article;
 
-
-
-BELOW NEW TABLE FOR P2
-
-CREATE TABLE article (
-id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
-content TEXT NOT NULL,
-photo VARCHAR(255) NOT NULL,
-category VARCHAR(100) NOT NULL,
-author VARCHAR(255) NOT NULL,
-date DATE NOT NULL
-);
-
-INSERT INTO ARTICLE (title, content, photo, category, author, date) 
-VALUES
-('Perdu en foret', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/28/4928/3264', 'Jurassique','Ben', '2023-04-18'),
-('Un peu d\'escalade', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/29/4000/2670', 'Jurassique','Laetitia', '2023-04-16'),
-('C\'est calme', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/135/2560/1920', 'Jurassique','Baptiste', '2023-04-15'),
-('Venez elle est bonne', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/179/2048/1365', 'Jurassique','Ben', '2023-04-13')
-;
-
-SELECT * FROM article
-ORDER BY date DESC
-LIMIT 3;
+SELECT * FROM admin;
+TRUNCATE TABLE admin;
+DROP TABLE admin;
 
 -- ADMIN
 
@@ -110,13 +85,6 @@ INSERT INTO `admin` (`username`, `type`, `password`) VALUES
 ('Admin2023', 'admin', '$2y$10$xQMlJsK6av7/J5KXFxp6KOb6lPxrbAhARIWmJQ8tCW2XWExF4rbJe');
 
 /* update following changes form articles*/ 
-
-USE projet_2;
-
-SELECT * FROM article;
-
-TRUNCATE TABLE article;
-DROP TABLE article;
 
 CREATE TABLE article (
 id INT PRIMARY KEY AUTO_INCREMENT,
