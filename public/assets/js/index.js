694ec60eab295141d1435f8aa9a7b8d6f7aa0aa5
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-04-27 14:03:52 +0200

    Cleaned the useless lightbox elements. Coded JS to display an image carousel onclick. Said pictures are now preloaded (head).

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,14 +1,19 @@
-
-import Lightbox from '/assets/js/bs5-lightbox.js';
-
-const options = {
-	keyboard: true,
-	size: 'lg',
-    constrain : true
-};
-
-document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventListener('click', (e) => {
-	e.preventDefault();
-	const lightbox = new Lightbox(el, options);
-	lightbox.show();
-}));
+function showSlider() {
+	var sliderContainer = document.querySelector('#carousel');
+	var sliderWrapper = document.querySelector('#slider-wrapper');
+	var images = sliderWrapper.querySelectorAll('img');
+	var activeIndex = 0;
+	
+	// Show the slider container
+	sliderContainer.style.display = 'block';
+	
+	// Show the first image
+	//images[activeIndex].classList.add('active');
+	
+	// Move to the next image on click
+	sliderContainer.addEventListener('click', function() {
+	  images[activeIndex].classList.remove('active');
+	  activeIndex = (activeIndex + 1) % images.length;
+	  images[activeIndex].classList.add('active');
+	});
+  }
