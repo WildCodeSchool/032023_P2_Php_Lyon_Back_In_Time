9e2b78598d7655f2cb0728bdf0fd3ec9a750c60d
Spolito24 <[baptisterenier69@gmail.com](mailto:baptisterenier69@gmail.com)>
2023-05-04 21:14:05 +0200

    some test on the category crud

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -23,21 +23,17 @@ class CategoryController extends AbstractController
 
     public function addCategory(): string
     {
-        $errors = [];
+        // $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
             $category = array_map('trim', $_POST);
             // $pictureService = new PictureService();
             // $pictureService->pictureFormFilter($picture);
             // $errors = $pictureService->errors;
-
-            if (empty($errors)) {
-                $categoryManager = new CategoryManager();
-                $categoryManager->insertCategory($category);
-
-                header('Location:/');
-                die();
-            }
+            // if (empty($errors)) {}
+            $categoryManager = new CategoryManager();
+            $categoryManager->insertCategory($category);
+            header('Location:/');
+            die();
         }
         if (isset($_SESSION['admin']) === true) {
             return $this->twig->render('Article/addCategory.html.twig');
@@ -51,8 +47,8 @@ class CategoryController extends AbstractController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = trim($_POST['id']);
-            $CategoryManager = new CategoryManager();
-            $CategoryManager->delete((int)$id);
+            $categoryManager = new CategoryManager();
+            $categoryManager->delete((int)$id);
 
             header('Location:/admin/management');
         }
