58fed869c280426ba462a66c31885a2c5997db53
Spolito24 <[baptisterenier69@gmail.com](mailto:baptisterenier69@gmail.com)>
2023-05-09 11:26:00 +0200

    do some fix and finsh the category crud

--- database.sql
+++ database.sql
@@ -1,67 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `simple-mvc`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
 USE projet_2;
 
 SELECT * FROM article;
@@ -84,36 +20,6 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`username`, `type`, `password`) VALUES
 ('Admin2023', 'admin', '$2y$10$xQMlJsK6av7/J5KXFxp6KOb6lPxrbAhARIWmJQ8tCW2XWExF4rbJe');
 
-/* update following changes form articles*/ 
-
-CREATE TABLE article (
-id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(255) NOT NULL,
-extract VARCHAR(100) NOT NULL,
-content TEXT NOT NULL,
-photo VARCHAR(255) NOT NULL,
-category VARCHAR(100) NOT NULL,
-author VARCHAR(255) NOT NULL,
-date DATE NOT NULL
-);
-
-INSERT INTO ARTICLE (title, extract, content, photo, category, author, date) 
-VALUES
-('Perdu en foret','luctus massa sit amet euismod luctus massa sit amet euismod', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/28/4928/3264', 'Jurassique','Ben', '2023-04-18'),
-('Un peu d\'escalade', 'luctus massa sit amet euismod luctus massa sit amet euismod', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/29/4000/2670', 'Jurassique','Laetitia', '2023-04-16'),
-('C\'est calme', 'luctus massa sit amet euismod','Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/135/2560/1920', 'Jurassique','Baptiste', '2023-04-15'),
-('Venez elle est bonne', 'luctus massa sit amet euismod ','Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'https://picsum.photos/id/179/2048/1365', 'Jurassique','Ben', '2023-04-13')
-;
-
-
-
-TRUNCATE TABLE picture;
-DROP TABLE picture;
-
-
-SELECT * FROM picture;
-
-
 CREATE TABLE picture (
 id INT PRIMARY KEY AUTO_INCREMENT,
 url VARCHAR(255) NOT NULL,
@@ -128,12 +34,6 @@ VALUES
 ('https://picsum.photos/id/179/2048/1365', 3)
 ;
 
-SELECT  a.title, p.url
-FROM picture as p
-	INNER JOIN article as a on p.article_id=a.id;
-    
-/* New table category & table article with foreign key category_id */ 
-
 CREATE TABLE category (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
@@ -146,10 +46,6 @@ VALUES
 ('Egypte Antique','/assets/images/egypte-antique.jpg'),
 ('Renaissance','/assets/images/renaissance.jpg')
 ;
-
-
-TRUNCATE TABLE article;
-DROP TABLE article;
 
 CREATE TABLE article (
 id INT PRIMARY KEY AUTO_INCREMENT,
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Model\CategoryManager;
 use App\Model\ArticleManager;
+use App\Service\CategoryService;
 
 class CategoryController extends AbstractController
 {
@@ -38,16 +39,19 @@ class CategoryController extends AbstractController
 
     public function addCategory(): string
     {
-        // $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $category = array_map('trim', $_POST);
+            $categoryService = new CategoryService();
+            $categoryService->categoryTitleErrors($category);
+            $categoryService->categoryPhotoErrors($category);
 
-            // TODO security
-
-            $categoryManager = new CategoryManager();
-            $categoryManager->insertCategory($category);
-            header('Location:/');
-            die();
+            if (empty($categoryService->errors)) {
+                $categoryManager = new CategoryManager();
+                $categoryManager->insertCategory($category);
+                header('Location:/');
+                die();
+            }
+            return $this->twig->render('Article/addCategory.html.twig', ['errors' => $categoryService->errors]);
         }
         if (isset($_SESSION['admin']) === true) {
             return $this->twig->render('Article/addCategory.html.twig');
@@ -62,20 +66,22 @@ class CategoryController extends AbstractController
         $categoryManager = new categoryManager();
         $category = $categoryManager->selectOneById($id);
 
-        // $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $category = array_map('trim', $_POST);
+            $categoryService = new CategoryService();
+            $categoryService->categoryTitleErrors($category);
+            $categoryService->categoryPhotoErrors($category);
 
-            // TODO security
-
-            $categoryManager->updateCategory($category);
-            header('Location:/category/list');
-            die();
-
-            // return $this->twig->render('Category/editCategory.html.twig', [
-            //     'category' => $category,
-            //     'errors' => $categoryService->errors
-            // ]);
+            if (empty($categoryService->errors)) {
+                $categoryManager = new CategoryManager();
+                $categoryManager->updateCategory($category);
+                header('Location:/category/list');
+                die();
+            }
+            return $this->twig->render('Article/editCategory.html.twig', [
+                'category' => $category,
+                'errors' => $categoryService->errors
+            ]);
         }
 
         if (isset($_SESSION['admin']) === true) {
@@ -88,14 +94,26 @@ class CategoryController extends AbstractController
         }
     }
 
-    public function delete(): void
+    public function delete()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = trim($_POST['id']);
             $categoryManager = new CategoryManager();
-            $categoryManager->delete((int)$id);
-
-            header('Location:/category/list');
+            $articleManager = new ArticleManager();
+            if (empty($articleManager->selectArticlesByCategory((int)$id))) {
+                $categoryManager->delete((int)$id);
+                header('Location:/category/list');
+                die();
+            } else {
+                $categoryManager = new CategoryManager();
+                $category = $categoryManager->selectAll();
+                $error = "Vous avez toujours des articles dans cette catégorie,
+                Veuillez supprimer ou changer de catégorie les articles";
+                return $this->twig->render('Article/categoryList.html.twig', [
+                    'category' => $category,
+                    'error' => $error
+                ]);
+            }
         }
     }
 }
